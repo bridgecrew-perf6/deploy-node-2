8a55682082d23c9b202da1365b136dd5c8fec43e
Everton Fernandes <evertonfernand23@gmail.com>
2022-01-27 21:19:40 -0300

    create project

---
+++ prisma/migrations/20220127235746_create_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
---
+++ src/server.js
@@ -0,0 +1,49 @@
+require("dotenv/config");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+
+const routes = express.Router();
+
+routes.get("/", async (_, res) => {
+  const posts = await prisma.post.findMany({
+    include: {
+      user: true,
+    },
+  });
+
+  return res.json({
+    result: posts,
+  });
+});
+
+routes.post("/", async (req, res) => {
+  const { name, title, body } = req.body;
+
+  const post = await prisma.post.create({
+    data: {
+      body,
+      title,
+      user: {
+        create: {
+          name,
+        },
+      },
+    },
+  });
+
+  return res.json({
+    result: post,
+  });
+});
+
+app.use("/post", routes);
+
+app.listen(3333, () => {
+  console.log("Server started");
+});
